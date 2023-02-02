35ff4644680b4890b0513ad4d47eb6b857fc0004
kevinnilsson <kevin7254@gmail.com>
2023-02-02 22:24:46 +0100

    Initial commit

---
+++ core/src/com/mygdx/game/Drop.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+public class Drop extends Game {
+    protected SpriteBatch batch;
+    protected BitmapFont font;
+    private MainMenuScreen mainMenuScreen;
+
+
+    @Override
+    public void create(){
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        mainMenuScreen = new MainMenuScreen(this);
+        this.setScreen(mainMenuScreen);
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+        mainMenuScreen.dispose();
+    }
+}
